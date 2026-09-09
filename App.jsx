c4c770f96f692ce64db0c38de1d1cd76acfaf231
chifukamwanza <chifukamwanza@gmail.com>
2026-09-09 20:05:14 +0200

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{padding: '40px', textAlign: 'center', fontFamily: 'sans-serif'}}>
-      <h1>🛍️ Shopline Zambia</h1>
-      <p>Buy & Sell marketplace for Africa</p>
-      <p>Site is now working! 🇿🇲</p>
-    </div>
-  )
-}
