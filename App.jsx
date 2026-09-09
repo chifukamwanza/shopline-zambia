709d0c00d098925cc2bfa77fb7314f9a25e78f5e
chifukamwanza <chifukamwanza@gmail.com>
2026-09-09 19:59:49 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{padding: '40px', textAlign: 'center', fontFamily: 'sans-serif'}}>
+      <h1>🛍️ Shopline Zambia</h1>
+      <p>Buy & Sell marketplace for Africa</p>
+      <p>Site is now working! 🇿🇲</p>
+    </div>
+  )
+}
